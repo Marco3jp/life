bcd608d360ebbeb603ac095f7ace793fdfe450c1
marco <28888207+Marco3jp@users.noreply.github.com>
2019-08-05 21:07:38 +0900

    optionがあったときにsessionStorageに格納するようにした

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import Item from "./item/item";
 import _Location from "./location/location";
 import Action from "./action/action";
 import _Event from "./event/event";
+import {Option} from "./model/option";
 
 class Life {
     load: Load;
@@ -11,11 +12,29 @@ class Life {
     action: Action;
     event: _Event;
 
-    constructor() {
+    constructor(option?: Option) {
         this.load = new Load();
         this.item = new Item();
         this.location = new _Location();
         this.action = new Action();
         this.event = new _Event();
+
+        Life.storeOptions(option);
     }
+
+    /**
+     * ゲームのバージョンやリソースのバージョンをセッションストレージに記録
+     * マイグレーションやキャッシュのチェックなどに使う
+     */
+    static storeOptions(option) {
+        if (option !== undefined) {
+            if (option.version !== undefined) {
+                sessionStorage.setItem("version", option.version);
+            }
+            if (option.resourceVersion !== undefined) {
+                sessionStorage.setItem("resourceVersion", option.resourceVersion);
+            }
+        }
+    }
+
 }
---
+++ model/option.ts
@@ -0,0 +1,4 @@
+export interface Option {
+    version?: string
+    resourceVersion?: string
+}
