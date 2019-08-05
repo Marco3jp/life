cb4d58f8826a83763ba299f4bdc351bcc29ba599
marco <28888207+Marco3jp@users.noreply.github.com>
2019-08-06 01:44:45 +0900

    itemとlocationにscriptを追加

--- model/item.ts
+++ model/item.ts
@@ -1,6 +1,9 @@
+import {Script} from "./script";
+
 export interface Item {
     id: number
     name: string
     require: Function // if return true, you can use it.
     effect: Function
+    script: Script
 }
--- model/location.ts
+++ model/location.ts
@@ -1,7 +1,9 @@
 import {Coordinate} from "./coordinate";
+import {Script} from "./script";
 
 export interface Location {
     id: number
     name: string
     coordinate: Coordinate
+    script: Script
 }
