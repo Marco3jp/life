0b54bf1269bd5f33755610a5d2767fcac4645e45
marco <28888207+Marco3jp@users.noreply.github.com>
2019-08-06 00:31:30 +0900

    オプションからロードするリソースを一括で指定できるようにしました

--- model/option.ts
+++ model/option.ts
@@ -1,4 +1,7 @@
+import {ResourceURIList} from "./resourceList";
+
 export interface Option {
     version?: string
     resourceVersion?: string
+    resourceList?: ResourceURIList
 }
---
+++ model/resourceList.ts
@@ -0,0 +1,7 @@
+export interface ResourceURIList {
+    itemURIList: Array<string>
+    mapURIList: Array<string>
+    eventURIList: Array<string>
+    scriptURIList: Array<string>
+    characterURIList: Array<string>
+}
