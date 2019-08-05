62b6e61f8b25db85551e31a0ca91baf39b215ca6
marco <28888207+Marco3jp@users.noreply.github.com>
2019-08-06 00:35:19 +0900

    itemのロードを実装

--- item/item.ts
+++ item/item.ts
@@ -1,3 +1,23 @@
-export default class Item {
+import localForage from "localforage";
+import {Item as ItemModel} from "../model/item"
 
+export default class Item {
+    private itemDb: LocalForage;
+
+    constructor() {
+        this.itemDb = localForage.createInstance({
+            name: "item"
+        })
+    }
+
+    load(url: string) {
+        fetch(url).then((response) => {
+            return response.json()
+        }).then(result => {
+            result.items.foreach((item: ItemModel) => {
+                this.itemDb.setItem(item.id.toString(), item)
+            })
+        })
+
+    }
 }
