5101b17dbb129fa44ba051d5cca7a0ef56a7450a
marco <28888207+Marco3jp@users.noreply.github.com>
2019-08-06 00:36:24 +0900

    locationのロードを実装

--- location/location.ts
+++ location/location.ts
@@ -1,3 +1,22 @@
-export default class _Location {
+import localForage from "localforage";
+import {Location as LocationModel} from "../model/location";
 
+export default class _Location {
+    private locationDb: LocalForage;
+
+    constructor() {
+        this.locationDb = localForage.createInstance({
+            name: "location"
+        })
+    }
+
+    load(url: string) {
+        fetch(url).then((response) => {
+            return response.json();
+        }).then(result => {
+            result.locations.foreach((location: LocationModel) => {
+                this.locationDb.setItem(location.id.toString(), location);
+            })
+        })
+    }
 }
