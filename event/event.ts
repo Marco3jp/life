1d3bdea76eda5d29f3a6ef1dc7a17ffe75f10ecc
marco <28888207+Marco3jp@users.noreply.github.com>
2019-08-11 05:56:59 +0900

    eventのloadを実装

--- event/event.ts
+++ event/event.ts
@@ -1,3 +1,25 @@
-export default class _Event {
+import localForage from "localforage";
+import {Event as EventModel} from "../model/event";
+import {parseJsonFunction} from "../module/parseJsonFunction";
 
+export default class _Event {
+    private eventDb: LocalForage;
+
+    constructor() {
+        this.eventDb = localForage.createInstance({
+            name: "event"
+        })
+    }
+
+    load(url: string) {
+        fetch(url).then((response) => {
+            return response.text()
+        }).then(raw => {
+            return JSON.parse(raw, parseJsonFunction)
+        }).then(result => {
+            result.events.foreach((event: EventModel) => {
+                this.eventDb.setItem(event.id.toString(), event)
+            })
+        })
+    }
 }
