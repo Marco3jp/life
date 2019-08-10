932253462f9412a9af4eaee8688f90552b828ffd
marco <28888207+Marco3jp@users.noreply.github.com>
2019-08-11 05:52:52 +0900

    actionのロードを実装

--- action/action.ts
+++ action/action.ts
@@ -1,3 +1,26 @@
-export default class Action {
+import localForage from "localforage";
+import {Action as ActionModel} from "../model/action"
+import {parseJsonFunction} from "../module/parseJsonFunction";
 
+export default class Action {
+    private actionDb: LocalForage;
+
+    constructor() {
+        this.actionDb = localForage.createInstance({
+            name: "action"
+        })
+    }
+
+    load(url: string) {
+        fetch(url).then((response) => {
+            return response.text()
+        }).then(raw => {
+            return JSON.parse(raw, parseJsonFunction)
+        }).then(result => {
+            result.items.foreach((action: ActionModel) => {
+                this.actionDb.setItem(action.id.toString(), action)
+            })
+        })
+
+    }
 }
