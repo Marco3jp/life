b64618235341d933bfab75acb9f95b241056ac17
marco <28888207+Marco3jp@users.noreply.github.com>
2019-08-12 02:32:49 +0900

    storeOptionsは引数必須なのでundefinedチェックを行い、リソースリストがある場合にリソースをまとめてロードするように修正

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import _Location from "./location/location";
 import Action from "./action/action";
 import _Event from "./event/event";
 import {Option} from "./model/option";
+import {ResourceURIList} from "./model/resourceList";
 
 class Life {
     item: Item;
@@ -16,7 +17,12 @@ class Life {
         this.action = new Action();
         this.event = new _Event();
 
-        Life.storeOptions(option);
+        if (option !== undefined) {
+            Life.storeOptions(option);
+            if (option.resourceList !== undefined) {
+                this.loadResources(option.resourceList);
+            }
+        }
     }
 
     /**
@@ -34,4 +40,26 @@ class Life {
         }
     }
 
+    loadResources(resourceList: ResourceURIList) {
+        if (resourceList.itemURIList !== undefined) {
+            resourceList.itemURIList.forEach((itemURI: string) => {
+                this.item.load(itemURI);
+            })
+        }
+        if (resourceList.locationURIList !== undefined) {
+            resourceList.locationURIList.forEach((locationURI: string) => {
+                this.location.load(locationURI);
+            })
+        }
+        if (resourceList.actionURIList !== undefined) {
+            resourceList.actionURIList.forEach((actionURI: string) => {
+                this.action.load(actionURI);
+            })
+        }
+        if (resourceList.eventURIList !== undefined) {
+            resourceList.eventURIList.forEach((eventURI: string) => {
+                this.event.load(eventURI);
+            })
+        }
+    }
 }
