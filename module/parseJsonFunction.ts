96b8f85b2eca9c2da22dcbdc271be69cc5b4538c
marco <28888207+Marco3jp@users.noreply.github.com>
2019-08-12 02:03:05 +0900

    アロー関数文字列にも対応

--- module/parseJsonFunction.ts
+++ module/parseJsonFunction.ts
@@ -1,5 +1,5 @@
 export function parseJsonFunction(key, value) {
-    if (typeof value === "string" && value.match(/^function/)) {
+    if (typeof value === "string" && (value.match(/^function/) || value.match(/^\(.*\) *=> *{(.*\n?)*}/))) {
         return Function.call(this, "return " + value)();
     }
     return value;
