db8f931c2cdf09c0b0a5480c8b14dcf9d87b964f
marco <28888207+Marco3jp@users.noreply.github.com>
2019-08-13 16:04:18 +0900

    値を代入するとlocalstorageに格納し、値を受け取るとlocalstorageから受け取るproxyを生成する関数を実装

---
+++ module/localStorageHandlerGenerator.ts
@@ -0,0 +1,18 @@
+export function generateLocalStorageHandler(prefix, key): ProxyHandler<any> {
+    const valueName = prefix + key;
+    return {
+        get: (target, name) => {
+            const value = localStorage.getItem(valueName);
+            return value !== null ? value : undefined;
+        },
+        set: (obj, prop, value) => {
+            try {
+                localStorage.setItem(valueName, value);
+            } catch (e) {
+                // safari in private mode or storage is full.
+                return false;
+            }
+            return true;
+        }
+    }
+}
