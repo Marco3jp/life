f894efed97145d834434ce3c40db25eea7d38902
marco <28888207+Marco3jp@users.noreply.github.com>
2019-08-13 17:41:03 +0900

    defaultLifeStatusを実装

---
+++ params/default/lifeStatus.ts
@@ -0,0 +1,27 @@
+import {HandleDefineder} from "../../model/handleDefineder";
+
+export let defaultLifeStatus: Array<HandleDefineder> = [{
+    key: "health",
+    validator: (value: string): string => {
+        if (parseFloat(value) > 100) {
+            return "100"
+        }
+        return value;
+    }
+}, {
+    key: "hanger",
+    validator: (value: string): string => {
+        if (parseFloat(value) > 100) {
+            return "100"
+        }
+        return value;
+    }
+}, {
+    key: "thirst",
+    validator: (value: string): string => {
+        if (parseFloat(value) > 100) {
+            return "100"
+        }
+        return value;
+    }
+}];
