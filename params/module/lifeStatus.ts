50862bc047a7a6d4f99d2ccec15c85c287d9f0ae
marco <28888207+Marco3jp@users.noreply.github.com>
2019-08-13 16:41:07 +0900

    paramsのlifeStatusを実装

---
+++ params/module/lifeStatus.ts
@@ -0,0 +1,14 @@
+import {generateLocalStorageHandler} from "../../module/localStorageHandlerGenerator";
+
+class LifeStatus {
+    health;
+    hanger;
+    thirst;
+
+    constructor() {
+        const prefix = "life_lifeStatus_";
+        this.health = new Proxy({}, generateLocalStorageHandler(prefix, "health"));
+        this.hanger = new Proxy({}, generateLocalStorageHandler(prefix, "hanger"));
+        this.thirst = new Proxy({}, generateLocalStorageHandler(prefix, "thirst"));
+    }
+}
