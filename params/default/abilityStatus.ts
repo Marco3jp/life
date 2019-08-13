85ab5c54352ab3926661b6d79f4046d81ddf5d6d
marco <28888207+Marco3jp@users.noreply.github.com>
2019-08-13 18:03:33 +0900

    abilityStatusのデフォルト値を定義

---
+++ params/default/abilityStatus.ts
@@ -0,0 +1,7 @@
+import {HandleDefineder} from "../../model/handleDefineder";
+
+export let defaultAbilityStatus: Array<HandleDefineder> = [{
+    key: "strength",
+}, {
+    key: "intelligent",
+}];
