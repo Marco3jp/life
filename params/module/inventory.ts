00bf7d145cb27b1ac160cd22b30ebc2247fc6772
marco <28888207+Marco3jp@users.noreply.github.com>
2019-08-13 20:29:16 +0900

    inventoryを実装

---
+++ params/module/inventory.ts
@@ -0,0 +1,60 @@
+import {Item as ItemModel} from "../../model/item";
+import ViewScript from "../../model/viewScript";
+import {InventoryRecord} from "../../model/inventoryRecord";
+import Params from "../params";
+import {parseJsonFunction} from "../../module/util/parseJsonFunction";
+import {stringifyFunction} from "../../module/util/stringifyFunction";
+
+export default class Inventory {
+    inventory: Array<InventoryRecord>;
+    params: Params;
+
+    constructor(params) {
+        const saveData = localStorage.getItem("life_inventory");
+        this.params = params;
+        if (saveData !== null) {
+            this.inventory = JSON.parse(saveData, parseJsonFunction);
+        } else {
+            this.inventory = [];
+        }
+    }
+
+    addItem(item: ItemModel, number?: number) {
+        const index = this.inventory.findIndex((inventoryRecord: InventoryRecord) => {
+            return inventoryRecord.item.id === item.id;
+        });
+        if (index !== -1) {
+            if (typeof number !== "undefined") {
+                this.inventory[index].number += number;
+            } else {
+                this.inventory[index].number++;
+            }
+        } else {
+            this.inventory.push({item: item, number: typeof number !== "undefined" ? number : 1});
+        }
+
+        this.saveInventory();
+    }
+
+    getMyInventory(): Array<InventoryRecord> {
+        return this.inventory;
+    }
+
+    use(item: ItemModel): ViewScript {
+        const index = this.inventory.findIndex((inventoryRecord: InventoryRecord) => {
+            return inventoryRecord.item.id === item.id;
+        });
+        if (index !== -1 && this.inventory[index].number <= 0 && this.inventory[index].item.require(this.params)) {
+            this.inventory[index].number--;
+            this.inventory[index].item.effect(this.params);
+            this.saveInventory();
+            return {message: "success"};
+        } else {
+            return {message: "error"};
+        }
+    }
+
+    saveInventory() {
+        localStorage.setItem("life_inventory", JSON.stringify(this.inventory, stringifyFunction));
+    }
+}
