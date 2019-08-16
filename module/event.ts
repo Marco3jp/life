d9fec683ead7e410f8cd9847e9667ed0353270e6
marco <28888207+Marco3jp@users.noreply.github.com>
2019-08-16 19:13:13 +0900

    eventの処理を実装

--- module/event.ts
+++ module/event.ts
@@ -3,10 +3,12 @@ import {Event as EventModel} from "../model/event";
 import {parseJsonFunction} from "./util/parseJsonFunction";
 import Params from "./params";
 import ViewScript from "../model/viewScript";
+import viewScript from "../model/viewScript";
 
 export default class _Event {
     private eventDb: LocalForage;
     private currentlyEvents: Array<EventModel>;
+    private cacheTime: number;
     params: Params;
 
     constructor(params) {
@@ -28,13 +30,49 @@ export default class _Event {
         })
     }
 
-    isEventing(): boolean {
-        // TODO
-        return false;
+    isEventing(): Promise<boolean> {
+        this.currentlyEvents = [];
+        return this.eventDb.iterate((event: EventModel) => {
+            if (event.require(this.params)) {
+                this.currentlyEvents.push(event);
+            }
+        }).then(() => {
+            this.currentlyEvents.sort(((a: EventModel, b: EventModel) => {
+                if (typeof a.weight === "undefined" && typeof b.weight === "undefined") {
+                    return 0;
+                } else if (typeof b.weight === "undefined") {
+                    return 1;
+                } else if (typeof a.weight === "undefined") {
+                    return -1;
+                } else if (a.weight === b.weight) {
+                    return 0;
+                } else if (a.weight > b.weight) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }));
+            this.cacheTime = Date.now();
+            return this.currentlyEvents.length > 0;
+        });
     }
 
-    proceedCurrentlyEvent(): ViewScript {
-        // TODO
-        return {message: "mock"};
+    proceedCurrentlyEvent(force?: boolean): Array<ViewScript> {
+        let result: Array<viewScript> = [];
+
+        if (this.cacheTime > 10000 && (typeof force === "undefined" || !force)) {
+            this.isEventing().then(r => {
+                if (r) this.proceedCurrentlyEvent(true);
+            });
+        }
+
+        this.currentlyEvents.forEach((event: EventModel) => {
+            const viewScript = event.effect(this.params);
+            if (viewScript !== undefined) {
+                result.push(viewScript);
+            }
+        });
+
+        return result;
     }
 }
