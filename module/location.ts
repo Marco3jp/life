2c4e3e2f444b74a0f701d108c316c75cafd20efb
marco <28888207+Marco3jp@users.noreply.github.com>
2019-08-21 03:21:59 +0900

    paramsをlocationから触れるようにした

--- module/location.ts
+++ module/location.ts
@@ -1,16 +1,19 @@
 import localForage from "localforage";
 import {Location as LocationModel} from "../model/location";
 import viewScript from "../model/viewScript";
+import Params from "./params";
 import {HistoryEnum} from "../model/enum/historyEnum";
 import {parseJsonFunction} from "./util/parseJsonFunction";
 
 export default class _Location {
     private locationDb: LocalForage;
+    private params: Params;
 
-    constructor() {
+    constructor(params: Params) {
         this.locationDb = localForage.createInstance({
             name: "location"
-        })
+        });
+        this.params = params;
     }
 
     load(url: string) {
