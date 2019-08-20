259c5c8037861ad1c8c8da44a4f61c81cc4db942
marco <28888207+Marco3jp@users.noreply.github.com>
2019-08-21 04:20:25 +0900

    setting削除とimportの削除忘れの削除

--- module/params.ts
+++ module/params.ts
@@ -1,23 +1,19 @@
 import Life from "../index";
-import Item from "./item";
 import _Location from "./location";
 import Action from "./action";
 import _Event from "./event";
 import State from "./state";
-import Setting from "./setting";
 
 export default class Params {
     location: _Location;
     action: Action;
     event: _Event;
     state: State;
-    setting: Setting;
 
     constructor(life: Life) {
         this.action = life.action;
         this.event = life.event;
         this.state = life.state;
         this.location = life.location;
-        this.setting = life.setting;
     }
 }
