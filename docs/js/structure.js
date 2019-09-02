688cc8668223cd8cd186c13edf7c78967deb57de
marco <28888207+Marco3jp@users.noreply.github.com>
2019-09-02 22:35:36 +0900

    ドキュメントの雛形を作成

---
+++ docs/js/structure.js
@@ -0,0 +1,20 @@
+export let structure = [
+    {
+        name: "Life",
+        path: "life",
+        header: true,
+        isNotPage: true,
+        child: [
+            {
+                name: "概要",
+                fileName: "overview.html"
+            },
+        ]
+    },
+    {
+        name: "型",
+        fileName: "type.html",
+        header: true,
+        isNotPage: false,
+    }
+];
