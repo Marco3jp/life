412c56d2226341327c699d675cc7a93110a49106
marco <28888207+Marco3jp@users.noreply.github.com>
2019-09-04 01:48:02 +0900

    サイト構造を修正

--- docs/js/structure.js
+++ docs/js/structure.js
@@ -1,13 +1,17 @@
 export let structure = [
     {
         name: "Life",
-        path: "life",
+        path: "",
         header: true,
         isNotPage: true,
         child: [
-            {
+            /*{
                 name: "概要",
                 fileName: "overview.html"
+            },*/
+            {
+                name: "エコシステム",
+                fileName: "ecosystem.html"
             },
         ]
     },
@@ -16,5 +20,16 @@ export let structure = [
         fileName: "type.html",
         header: true,
         isNotPage: false,
-    }
+    },
+    {
+        name: "仕様以外",
+        path: "",
+        header: true,
+        isNotPage: true,
+        child: [{
+            name: "今後の方針など",
+            fileName: "todo.html"
+        }]
+    },
+
 ];
