2c55a38235388e4b33a3ab05b5014e24abbb1f76
marco <28888207+Marco3jp@users.noreply.github.com>
2019-09-25 02:43:49 +0900

    デフォルト値を設定できるように実装

--- module/state.ts
+++ module/state.ts
@@ -7,6 +7,9 @@ import {defaultLifeStatus} from "./state/default/lifeStatus";
 import {defaultAbilityStatus} from "./state/default/abilityStatus";
 import Params from "./params";
 import Values from "./state/values";
+import {DefinerURIs} from "../model/definerURIs";
+import {parseJsonFunction} from "./util/parseJsonFunction";
+import {HandleDefiner} from "../model/handleDefiner";
 
 export default class State {
     abilityStatus: AbilityStatus;
@@ -16,12 +19,62 @@ export default class State {
     lifeStatus: LifeStatus;
     values: Values;
 
-    constructor(params: Params) {
-        this.abilityStatus = new AbilityStatus(defaultAbilityStatus);
-        this.lifeStatus = new LifeStatus(defaultLifeStatus);
+    constructor(params: Params, definerURI?: DefinerURIs) {
+        this.initDefiner(definerURI);
         this.inventory = new Inventory(params);
         this.flag = new Flag();
         this.history = new History();
-        this.values = new Values([]);
+    }
+
+    initDefiner(definerURI) {
+        if (typeof definerURI !== "undefined") {
+            if (typeof definerURI.abilityStatusDefinerURI !== "undefined") {
+                this.fetchDefiner(definerURI.abilityStatusDefinerURI).then(abilityStatus => {
+                    this.loadAbilityStatusDefiner(abilityStatus);
+                });
+            } else {
+                this.loadAbilityStatusDefiner(defaultAbilityStatus);
+            }
+
+            if (typeof definerURI.lifeStatusDefinerURI !== "undefined") {
+                this.fetchDefiner(definerURI.lifeStatusDefinerURI).then(lifeStatus => {
+                    this.loadLifeStatusDefiner(lifeStatus);
+                });
+            } else {
+                this.loadLifeStatusDefiner(defaultLifeStatus);
+            }
+
+            if (typeof definerURI.valueDefinerURI !== "undefined") {
+                this.fetchDefiner(definerURI.valueDefinerURI).then(values => {
+                    this.loadValuesDefiner(values);
+                });
+            } else {
+                this.loadValuesDefiner([]);
+            }
+        } else {
+            this.loadAbilityStatusDefiner(defaultAbilityStatus);
+            this.loadLifeStatusDefiner(defaultLifeStatus);
+            this.loadValuesDefiner([]);
+        }
+    }
+
+    fetchDefiner(definerURI: string): Promise<Array<HandleDefiner>> {
+        return fetch(definerURI).then(response => {
+            return response.text();
+        }).then(raw => {
+            return JSON.parse(raw, parseJsonFunction);
+        });
+    }
+
+    loadAbilityStatusDefiner(abilityStatus: Array<HandleDefiner>) {
+        this.abilityStatus = new AbilityStatus(abilityStatus);
+    }
+
+    loadLifeStatusDefiner(lifeStatus: Array<HandleDefiner>) {
+        this.lifeStatus = new LifeStatus(lifeStatus);
+    }
+
+    loadValuesDefiner(values: Array<HandleDefiner>) {
+        this.values = new Values(values);
     }
 }
