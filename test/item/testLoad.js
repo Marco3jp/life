16b4a7c547389e3d00903af992e66350158cdbc9
marco <28888207+Marco3jp@users.noreply.github.com>
2019-09-30 23:40:03 +0900

    時間に依存しないファイル名に修正

--- test/item/testLoad.js
+++ test/item/testLoad.js
@@ -1,7 +1,7 @@
 export function testLoad() {
     describe("## load()", () => {
         it("テストデータの読み込み", (done) => {
-            item.load("./testResources/itemData_1569516268.json").then(result => {
+            item.load("./testResources/itemData.json").then(result => {
                 if (result) {
                     done();
                 } else {
