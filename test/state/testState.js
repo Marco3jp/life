fb097b97fbd5a7f9e89e03c3585630c8b883f603
marco <28888207+Marco3jp@users.noreply.github.com>
2019-10-01 10:43:28 +0900

    lifeStatusのテストを実行

--- test/state/testState.js
+++ test/state/testState.js
@@ -1,9 +1,11 @@
 import {testGameDate} from "./gameDate/testGameDate";
 import {testAbilityStatus} from "./abilityStatus/testAbilityStatus";
+import {testLifeStatus} from "./lifeStatus/testLifeStatus";
 
 export function testState() {
     describe("# State", () => {
         testAbilityStatus();
+        testLifeStatus();
         testGameDate();
     });
 }
