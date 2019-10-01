f392c5c6f8f02a24106d3aa60bf3fa9254229c1f
marco <28888207+Marco3jp@users.noreply.github.com>
2019-10-01 12:18:57 +0900

    Flagのtestを実行

--- test/state/testState.js
+++ test/state/testState.js
@@ -1,11 +1,13 @@
 import {testGameDate} from "./gameDate/testGameDate";
 import {testAbilityStatus} from "./abilityStatus/testAbilityStatus";
 import {testLifeStatus} from "./lifeStatus/testLifeStatus";
+import {testFlag} from "./flag/testFlag";
 
 export function testState() {
     describe("# State", () => {
         testAbilityStatus();
         testLifeStatus();
+        testFlag();
         testGameDate();
     });
 }
