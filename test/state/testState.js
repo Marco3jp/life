64bc0167aad6361297d82c89b8a1b97a7beb0fde
marco <28888207+Marco3jp@users.noreply.github.com>
2019-10-02 16:06:22 +0900

    Historyのテストを実行

--- test/state/testState.js
+++ test/state/testState.js
@@ -2,12 +2,14 @@ import {testGameDate} from "./gameDate/testGameDate";
 import {testAbilityStatus} from "./abilityStatus/testAbilityStatus";
 import {testLifeStatus} from "./lifeStatus/testLifeStatus";
 import {testFlag} from "./flag/testFlag";
+import {testHistory} from "./history/testHistory";
 
 export function testState() {
     describe("# State", () => {
         testAbilityStatus();
         testLifeStatus();
         testFlag();
+        testHistory();
         testGameDate();
     });
 }
