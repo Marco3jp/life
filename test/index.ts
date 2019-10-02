09daaa7b3e4ce7d0c42ad196fd29f9d16f551a31
marco <28888207+Marco3jp@users.noreply.github.com>
2019-10-02 16:03:41 +0900

    ロード周りのテストのために実行回数をLocalStorageに格納

--- test/index.ts
+++ test/index.ts
@@ -4,8 +4,14 @@ import {testEvent} from "./event/testEvent";
 import {testItem} from "./item/testItem";
 import {testLocation} from "./location/testLocation";
 
+init();
 testAction();
 testEvent();
 testItem();
 testLocation();
 testState();
+
+function init() {
+    const testNumber = localStorage.getItem("testNumber");
+    localStorage.setItem("testNumber", testNumber === null ? "1" : (parseInt(testNumber) + 1).toString());
+}
