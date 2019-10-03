a32fceaf2b3a7e6250001b4c3f86d5d630724305
marco <28888207+Marco3jp@users.noreply.github.com>
2019-10-04 02:42:22 +0900

    不要なdone()をしていたので削除

--- test/state/history/testPushAndGet.js
+++ test/state/history/testPushAndGet.js
@@ -5,22 +5,19 @@ import assert from "assert";
 
 export function testPushAndGet() {
     describe("### Push and Get", () => {
-        it("ACTIONに履歴を追加して一致しているかチェック", (done) => {
+        it("ACTIONに履歴を追加して一致しているかチェック", () => {
             _history.pushState(HistoryEnum.ACTION, sampleAction[0]);
             assert.strictEqual(_history.getLatestState(HistoryEnum.ACTION), sampleAction[0]);
-            done();
         });
 
-        it("EVENTに履歴を追加して一致しているかチェック", (done) => {
+        it("EVENTに履歴を追加して一致しているかチェック", () => {
             _history.pushState(HistoryEnum.EVENT, sampleEvent[0]);
             assert.strictEqual(_history.getLatestState(HistoryEnum.EVENT), sampleEvent[0]);
-            done();
         });
 
-        it("EVENTに履歴を追加して一致しているかチェック", (done) => {
+        it("EVENTに履歴を追加して一致しているかチェック", () => {
             _history.pushState(HistoryEnum.EVENT, sampleEvent[1]);
             assert.strictEqual(_history.getLatestState(HistoryEnum.EVENT), sampleEvent[1]);
-            done();
         });
     });
 }
