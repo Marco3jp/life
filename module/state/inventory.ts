1e5536d31c029a0d139a72f3913a303042e9551e
marco <28888207+Marco3jp@users.noreply.github.com>
2019-10-08 19:33:06 +0900

    inventoryReferenceをinventoryIndexに変更し、関連箇所を修正

--- module/state/inventory.ts
+++ module/state/inventory.ts
@@ -9,15 +9,16 @@ import {stringifyFunction} from "../util/stringifyFunction";
 // TODO: 引数をItemModelではなくてidにする
 export default class Inventory {
     inventory: Array<InventoryRecord>;
-    inventoryReference: Map<number, InventoryRecord>;
+    inventoryIndex: Map<number, number>; // id to index
     params: Params;
 
     constructor() {
+        this.inventoryIndex = new Map();
         const saveData = localStorage.getItem("life_inventory");
         if (saveData !== null) {
             this.inventory = JSON.parse(saveData, parseJsonFunction);
-            this.inventory.forEach(inventoryRecord => {
-                this.inventoryReference.set(inventoryRecord.item.id, inventoryRecord);
+            this.inventory.forEach((inventoryRecord, index) => {
+                this.inventoryIndex.set(inventoryRecord.item.id, index);
             })
         } else {
             this.inventory = [];
@@ -28,42 +29,65 @@ export default class Inventory {
         this.params = params;
     }
 
+    /**
+     * アイテムを新たにインベントリに加える。
+     * @param item
+     */
+    private push(item: InventoryRecord): number {
+        const index = this.inventory.push(item) - 1;
+        this.inventoryIndex.set(this.inventory[index].item.id, index);
+        this.saveInventory();
+        return index;
+    }
+
+    /**
+     * アイテムの数を加算する。アイテムがない場合はpush()にフォールバックする。
+     * @param item
+     * @param number
+     */
     addItem(item: ItemModel, number?: number) {
-        const index = this.inventory.findIndex((inventoryRecord: InventoryRecord) => {
-            return inventoryRecord.item.id === item.id;
-        });
-        if (index !== -1) {
+        const index: number | undefined = this.inventoryIndex.get(item.id);
+        if (typeof index !== "undefined") {
             if (typeof number !== "undefined") {
                 this.inventory[index].number += number;
             } else {
                 this.inventory[index].number++;
             }
         } else {
-            const index = this.inventory.push({item: item, number: typeof number !== "undefined" ? number : 1});
-            this.inventoryReference.set(this.inventory[index].item.id, this.inventory[index]);
+            this.push({item: item, number: typeof number !== "undefined" ? number : 1});
         }
 
         this.saveInventory();
     }
 
+    /**
+     * インベントリをまるごと返す。
+     */
     getMyInventory(): Array<InventoryRecord> {
         return this.inventory;
     }
 
+    /**
+     * 引数のアイテムの数を返す。不当なIDは持っていない扱いで0を返す。
+     * @param id
+     */
     getNumberOfItem(id: number): number {
-        let inventoryRecord = this.inventoryReference.get(id);
-        if (typeof inventoryRecord !== "undefined") {
-            return inventoryRecord.number;
+        const index = this.inventoryIndex.get(id);
+        if (typeof index !== "undefined") {
+            return this.inventory[index].number;
         } else {
             return 0;
         }
     }
 
-    use(item: ItemModel): ViewScript {
-        const index = this.inventory.findIndex((inventoryRecord: InventoryRecord) => {
-            return inventoryRecord.item.id === item.id;
-        });
-        if (index !== -1 && this.inventory[index].number <= 0 && this.inventory[index].item.require(this.params)) {
+    /**
+     * アイテムを使用する。
+     * @param id
+     */
+    // TODO: 消費数を任意個許容する
+    use(id: number): ViewScript {
+        const index = this.inventoryIndex.get(id);
+        if (typeof index !== "undefined" && this.inventory[index].number >= 1 && this.inventory[index].item.require(this.params)) {
             this.inventory[index].number--;
             this.inventory[index].item.effect(this.params);
             this.saveInventory();
@@ -74,15 +98,25 @@ export default class Inventory {
         }
     }
 
+    /**
+     * アイテムを消費する。useと異なりeffectを実行しない。
+     * @param id
+     * @param number
+     */
+    // TODO: 減らす数をundefinableにする
     spendItem(id: number, number: number): boolean {
-        let inventoryRecord = this.inventoryReference.get(id);
-        if (typeof inventoryRecord !== "undefined" && inventoryRecord.number >= number) {
-            inventoryRecord.number -= number;
+        const index = this.inventoryIndex.get(id);
+        if (typeof index !== "undefined" && this.inventory[index].number >= number) {
+            this.inventory[index].number -= number;
+            this.saveInventory();
             return true;
         }
         return false;
     }
 
+    /**
+     * インベントリ情報をlocalstorageに保存する。
+     */
     private saveInventory() {
         localStorage.setItem("life_inventory", JSON.stringify(this.inventory, stringifyFunction));
     }
